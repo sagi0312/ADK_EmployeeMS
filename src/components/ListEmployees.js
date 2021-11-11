96a2f44a738ed861adbadbaeda032c0166d8a715
Anju Karanji <anjukaranji@gmail.com>
2021-11-11 15:27:42 -0500

    First reactjs commit

---
+++ src/components/ListEmployees.js
@@ -0,0 +1,16 @@
+const ListEmployees = () => {
+  return (
+    <>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light"></nav>
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          ADK Inc
+        </a>
+      </div>
+
+      <h2 className="text-center">Employees</h2>
+    </>
+  );
+};
+
+export default ListEmployees;
