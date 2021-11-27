65f65370c58b59ea5a64a9e055f2594cafd75278
Anju Karanji <anjukaranji@gmail.com>
2021-11-26 19:22:01 -0500

    Table Created

--- src/components/ListEmployees.js
+++ src/components/ListEmployees.js
@@ -1,4 +1,18 @@
-const ListEmployees = () => {
+function ListEmployees() {
+  const employeeArray = [
+    {
+      id: "1",
+      fName: "Lotus",
+      lName: "baba",
+      email: "ggg@jjj",
+    },
+    {
+      id: "2",
+      fName: "Rose",
+      lName: "cada",
+      email: "uuu@ppp",
+    },
+  ];
   return (
     <>
       {/* NAVBAR */}
@@ -27,9 +41,50 @@ const ListEmployees = () => {
         </div>
       </nav>
       {/* SHOWCASE */}
-      <h2 className="text-center">Employees</h2>
+      <section className="bg-secondary text-light p-5 text-center text-sm-start">
+        <div className="container">
+          <div class="d-sm-flex align-items-center justify-content-between">
+            <img
+              src="images/Main-office-icon.svg"
+              alt=""
+              class="img-fluid d-none d-sm-block news-input"
+            />
+            <div>
+              <h1>
+                <span class="text-warning">Employee</span> Database
+              </h1>
+              <p class="lead my-4">
+                Created using Spring boot + reactjs + bootstrap 5
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* TABLE OF EMPLOYEES */}
+      <div class="mx-4 my-4">
+        <table class="table table-striped table-bordered table-sm">
+          <thead>
+            <tr>
+              <th scope="col">Id</th>
+              <th scope="col">Employee First Name</th>
+              <th scope="col">Employee Last Name</th>
+              <th scope="col">Email</th>
+              <th scope="col">Actions</th>
+            </tr>
+          </thead>
+
+          {employeeArray.map((emp) => (
+            <tbody>
+              <th scope="col">{emp.id}</th>
+              <th scope="col">{emp.fName}</th>
+              <th scope="col">{emp.lName}</th>
+              <th scope="col">{emp.email}</th>
+            </tbody>
+          ))}
+        </table>
+      </div>
     </>
   );
-};
+}
 
 export default ListEmployees;
