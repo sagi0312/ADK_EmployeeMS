599369c7806b35819d062e376e662327e50acdec
Anju Karanji <anjukaranji@gmail.com>
2021-11-27 01:25:26 -0500

    Added React- Router-Dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import ListEmployees from "./components/ListEmployees";
+import Anju from "./components/Anju";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <>
-      <ListEmployees />
+      <Router>
+        <Routes>
+          <Route path="/" element={<ListEmployees />} />
+
+          <Route path="/about" element={<Anju />} />
+        </Routes>
+      </Router>
     </>
   );
 };
---
+++ src/components/Anju.js
@@ -0,0 +1,11 @@
+function Anju() {
+  return (
+    <>
+      <div className="container">
+        <h2 className="text-center">This is About me Page</h2>
+      </div>
+    </>
+  );
+}
+
+export default Anju;
